8f0ec48a067b01ae067ab57e60b529e52c63b2c7
Nils Langner <nils.langner@leankoala.com>
2019-02-01 17:25:39 +0100

    Check: FileCreatedAfterCheck

---
+++ examples/fileCreatedAfter.php
@@ -0,0 +1,16 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+$foundation = new \Leankoala\HealthFoundation\HealthFoundation();
+
+// max disc usage 95%
+$fileCreatedAfterCheck = new \Leankoala\HealthFoundation\Check\Files\FileCreatedAfterCheck();
+$fileCreatedAfterCheck->init('/leankoala/storage/file/03/72', new DateTime("-1 year"), '*.json');
+
+$foundation->registerCheck($fileCreatedAfterCheck);
+
+$runResult = $foundation->runHealthCheck();
+
+$formatter = new \Leankoala\HealthFoundation\Result\Format\Ietf\IetfFormat();
+$formatter->handle($runResult, 'Backup file was created successfully.', 'Seems like the backup script does not create new archives.');
---
+++ src/Check/Files/FileCreatedAfterCheck.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Leankoala\HealthFoundation\Check\Files;
+
+use Leankoala\HealthFoundation\Check\Check;
+use Leankoala\HealthFoundation\Check\Result;
+
+class FileCreatedAfterCheck implements Check
+{
+    const IDENTIFIER = 'base:files:newerThan';
+
+    private $directory;
+
+    private $pattern;
+
+    /**
+     * @var \DateTime
+     */
+    private $date;
+
+    public function run()
+    {
+        $files = glob(rtrim($this->directory, '/') . DIRECTORY_SEPARATOR . $this->pattern);
+
+        foreach ($files as $file) {
+            if (is_file($file)) {
+                if (filemtime($file) >= $this->date->getTimestamp()) {
+                    return new Result(Result::STATUS_PASS, 'At least one file (' . $file . ') was found newer than ' . $this->date->format('Y-m-d H:m:s') . '.');
+                }
+            }
+        }
+
+        return new Result(Result::STATUS_FAIL, 'No file was found newer than ' . $this->date->format('Y-m-d H:m:s') . '.');
+    }
+
+    public function init($directory, \DateTime $date, $pattern = '*')
+    {
+        $this->directory = $directory;
+        $this->date = $date;
+        $this->pattern = $pattern;
+    }
+
+    public function getIdentifier()
+    {
+        return self::IDENTIFIER;
+    }
+}
